7c3e9db9783344cc71a4571209678e1cdc90a767
leehunkyu <leehunkyu00@gmail.com>
2017-05-20 16:55:01 +0900

    Init commit with created project.

---
+++ src/main/java/com/chatsample/ChatsampleApplication.java
@@ -0,0 +1,12 @@
+package com.chatsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatsampleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/chatsample/ChatsampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chatsample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChatsampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
